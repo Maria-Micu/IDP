62548d6b182e7ed3ae3397596ce6dd353b3e5e0a
Maria Micu <maria.micu@stud.acs.upb.ro>
2020-04-26 00:45:19 +0300

    Update login.sql

--- sql-scripts/login.sql
+++ sql-scripts/login.sql
@@ -1,10 +1,10 @@
 CREATE TABLE login (
-	id int(3),
+	guid int(3),
 	user varchar(25),
 	password varchar(25)
 );
 
-INSERT INTO login (id, user, password)
+INSERT INTO login (guid, user, password)
 VALUES (1, 'mariamicu','1234567');
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'admin';
